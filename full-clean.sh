d9095e9b61b5b9c11ac5e1619912e970d5acd3c7
Fabio Alessandro Locati <fabio@locati.cc>
2016-02-09 19:23:39 +0100

    Polishing

--- full-clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-. ./env.sh
-
-make -C assets clean
-rm -rf bin pkg logs *.log
-rm -rf src/github.com src/launchpad.net src/code.google.com src/golang.org src/mock
-tar zxvf dependencies.tar.gz
-go clean ./...
